bd9069a622567129bad3cf7291557f063ae45799
Aleksander Szut <szut.aleksander@gmail.com>
2023-05-27 15:53:05 +0200

    feature(assert): add assert class and assert test

---
+++ src/Shared/Domain/Exception/InvalidObjectInstanceException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Src\Shared\Domain\Exception;
+
+use InvalidArgumentException;
+
+class InvalidObjectInstanceException extends InvalidArgumentException
+{
+
+    private const ERROR_MESSAGE = 'The object <%s> is not an instance of <%s>';
+
+    public function __construct(object $object, string $instance)
+    {
+        parent::__construct(
+            sprintf(
+                self::ERROR_MESSAGE,
+                $instance,
+                $object::class
+            )
+        );
+    }
+
+}
