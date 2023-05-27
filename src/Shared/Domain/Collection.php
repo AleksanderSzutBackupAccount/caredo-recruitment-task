d6bb0106f92177215c66352ec56abf611dec658d
Aleksander Szut <szut.aleksander@gmail.com>
2023-05-27 16:34:07 +0200

    feature(collection): add Collection class

---
+++ src/Shared/Domain/Collection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Src\Shared\Domain;
+
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+
+abstract readonly class Collection implements Countable, IteratorAggregate
+{
+    public function __construct(public array $items)
+    {
+        Assert::arrayOf($this->type(), $items);
+    }
+
+    abstract protected function type(): string;
+
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+}
