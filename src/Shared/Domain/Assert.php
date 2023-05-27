0d46b9c881a32fcda749b48060fda2fb8d32667e
Aleksander Szut <szut.aleksander@gmail.com>
2023-05-27 15:52:55 +0200

    feature(assert): add assert class and assert test

---
+++ src/Shared/Domain/Assert.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Src\Shared\Domain;
+
+use InvalidArgumentException;
+use Src\Shared\Domain\Exception\InvalidObjectInstanceException;
+
+final class Assert
+{
+    public static function arrayOf(string $class, array $items): void
+    {
+        foreach ($items as $item) {
+            self::instanceOf($class, $item);
+        }
+    }
+
+    public static function instanceOf(string $class, object $item): void
+    {
+        if (!$item instanceof $class) {
+            throw new InvalidObjectInstanceException( $item, $class);
+        }
+    }
+}
---
+++ tests/Unit/Shared/AssertTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit\Shared;
+
+use PHPUnit\Framework\TestCase;
+use Src\Shared\Domain\Assert;
+use Src\Shared\Domain\Exception\InvalidObjectInstanceException;
+use stdClass;
+
+class AssertTest extends TestCase
+{
+    public function test_instance_of_throw_error(): void
+    {
+        $object1 = new stdClass();
+        $object2 = new class {};
+        $this->expectException(InvalidObjectInstanceException::class);
+
+        Assert::instanceOf($object1::class, $object2);
+    }
+    public function test_instance_of_not_throw_error(): void
+    {
+        $object1 = new stdClass();
+
+        Assert::instanceOf($object1::class, $object1);
+
+        $this->assertTrue(true);
+    }
+
+}
